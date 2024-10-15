6b8088a47fe5d85ca560f88063fd5f7fd449a5dc
Hajarig <klk@gmail.com>
2024-10-15 12:13:55 +0200

    entre guillemets

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+# Demander à l'utilisateur d'entrer deux nombres
+num1 = float(input("Entrez le premier nombre : "))
+num2 = float(input("Entrez le deuxième nombre : "))
+
+# Multiplier les deux nombres
+resultat = num1 * num2
+
+# Afficher le résultat
+print(f"Le résultat de la multiplication de {num1} et {num2} est : {resultat}")
